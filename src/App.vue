31d2633d47f20f69d2c67494ad28ec7746429d05
xyy <1312936448@qq.com>
2026-04-23 15:08:07 +0800

    feat: 这是我的第一个vue项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Demo />
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "App",
+});
+import Demo from "./components/Demo.vue";
+</script>
+
+<style></style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>这是新的页面</h2>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "Demo",
+});
+</script>
+
+<style></style>
